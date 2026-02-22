8c1d0c7954dbbf885db55eea17c6f4b5cbfd2fdd
prabhat <giradeprabhat88@gmail.com>
2026-02-23 00:29:55 +0530

    initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        int sum = a + b;
+
+        System.out.println("Sum = " + sum);
+    }
+}
