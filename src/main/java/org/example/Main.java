a93c5cf031e38a40e1249143cd8e5e99cdce4ec9
prabhat <giradeprabhat88@gmail.com>
2026-02-23 01:13:31 +0530

    initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,17 @@
 package org.example;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+
         int a = 10;
         int b = 5;
-
         int sum = a + b;
 
         System.out.println("Sum = " + sum);
+
+        // keep container running
+        while (true) {
+            Thread.sleep(10000);
+        }
     }
 }
